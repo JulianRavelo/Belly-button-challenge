a9cf783028b019b8f51c6715cf6e59bc81d3fb7b
JulianRavelo <oscarj.9430@gmail.com>
2023-10-26 07:06:54 +0800

    progress 2

--- static/js/app.js
+++ static/js/app.js
@@ -2,35 +2,39 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function (data) {
+d3.json(url).then(function getAPI(data) {
     console.log(data);
-    console.log(data.samples[0].sample_values[0]);
-    let names = [];
-    let sample_values = [];
-    let otu_ids = [];
-    for (let i = 0; i < data.names.length; i++){
-        names.push(data.names[i]);
-        sample_values.push(data.samples[i].sample_values);
-        otu_ids.push(data.samples[i].otu_ids);
-    }
-    // // Sort the data by sample_values descending
-    // slicedData = data.samples.slice(0,10);
-    // console.log(slicedData);
-
-    // // Reverse the array to accommodate Plotly's defaults
-    // reversedData = slicedData.reverse();
-    // console.log(reversedData);
-    console.log(otu_ids);
-    let trace1 = {
-        x: names,
-        y: sample_values,
-        type: "bar"
-    }; 
-
-    let dataArray = [trace1];
-
-    Plotly.newPlot("plot", dataArray);
 });
+
+getAPI(url);
+
+
+// console.log(data.samples[0].sample_values[0]);
+//     let names = [];
+//     let sample_values = [];
+//     let otu_ids = [];
+//     for (let i = 0; i < data.names.length; i++){
+//         names.push(data.names[i]);
+//         sample_values.push(data.samples[i].sample_values);
+//         otu_ids.push(data.samples[i].otu_ids);
+//     }
+//     // // Sort the data by sample_values descending
+//     // slicedData = data.samples.slice(0,10);
+//     // console.log(slicedData);
+
+//     // // Reverse the array to accommodate Plotly's defaults
+//     // reversedData = slicedData.reverse();
+//     // console.log(reversedData);
+//     console.log(otu_ids);
+
+//     let trace1 = {
+//         x: names,
+//         y: sample_values,
+//         type: "bar"
+//     }; 
+//     let dataArray = [trace1];
+    
+//     Plotly.newPlot("plot", dataArray);
 
     // let trace1 = {
     //     x: data.map(row => row.samples.sample_values),
